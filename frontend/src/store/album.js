38243d297324910893722c82fb16464fe27b442d
IvanGent <gentilleivan@gmail.com>
2021-02-04 05:32:39 -0500

    getting album state

--- frontend/src/store/album.js
+++ frontend/src/store/album.js
@@ -10,25 +10,50 @@ const addPhoto = (photo) => {
     }
 }
 
-const deletePhoto = (photo) => {
+const deletePhoto = (i) => {
     return {
         type: DELETE_PHOTO,
-        payload: photo
+        payload: i
     }
 }
 
-const addingPhoto = (data) => async(dispatch) => {
-    const { userId, photoId, albumId } = data;
-    const res = await fetch(`/api/album/`)
+export const addingPhoto = (data) => async(dispatch) => {
+    const { photoId, albumId } = data;
+    const res = await fetch(`/api/album/photo`, {
+        method: 'POST',
+        body: JSON.stringify({
+            photoId,
+            albumId
+        })
+    })
+    await dispatch(addPhoto(res.data.photo))
+    return res;
+}
+
+export const removingPhoto = (data) => async(dispatch) => {
+    const { photoId, albumId } = data;
+    const res = await fetch(`/api/album/photo`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            photoId,
+            albumId
+        })
+    })
+    await dispatch(deletePhoto(res.data.photo))
+    return res
 }
 
 let initialState = { album: [] };
 
-const albumReducer = (state = initialState, { type, payload}) => {
+const albumReducer = (state = initialState, { type, payload }) => {
     let newState;
     switch(type) {
         case ADD_PHOTO:
             newState = { album: [...state.album, payload]}
+            return newState;
+        case DELETE_PHOTO:
+            let arr = state.filter((ele, i) => i !== payload)
+            newState = { album: arr}
             return newState;
         default:
             return state;
