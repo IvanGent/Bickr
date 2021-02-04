05cf94014bfebffc08d465b716b5aba1642d8bda
IvanGent <gentilleivan@gmail.com>
2021-02-04 02:34:27 -0500

    started Album compo

---
+++ frontend/src/components/Album/index.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as albumActions from '../../store/album';
+import { useParams } from 'react-router-dom';
+import { fetch } from '../../store/csrf';
+
+
+const Album = () => {
+
+
+    return (
+        <div></div>
+    )
+}
+
+export default Album;
