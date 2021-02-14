aa8db7bda9f58d6c97db1a4605fd708347dd65c5
IvanGent <gentilleivan@gmail.com>
2021-02-14 14:13:57 -0500

    displaying album

--- frontend/src/components/ShowingAlbum/index.js
+++ frontend/src/components/ShowingAlbum/index.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import { NavLink } from 'react-router-dom';
 import { fetch } from '../../store/csrf';
+import './ShowingAlbum.css'
 
 
 const ShowngAlbum = ({ album }) => {
@@ -18,25 +20,26 @@ const ShowngAlbum = ({ album }) => {
     }, [])
 
     return (
-        <div>
-            <h1>{albumInfo.name}</h1>
-            {albumPhotos.length > 0 ? (
-                <div>
-                    {albumPhotos.map(ele => (
-                        <div key={ele.Photo.id}>
-                            <img id={ele.Photo.id} src={ele.Photo.thumbSrc} alt='' />
+        <div className='AlbumCont'>
+            <div className='albumContents'>
+                <h1>{albumInfo.name}</h1>
+                {/* <div className='albumPhotos'> */}
+                    {albumPhotos.length > 0 ? (
+                        <div className='albumPhotos'>
+                            {albumPhotos.map(ele => (
+                                <div key={ele.Photo.id} className='image'>
+                                    <NavLink to={`/photo/${ele.Photo.id}`} className='imageLink' />
+                                    <img id={ele.Photo.id} src={ele.Photo.thumbSrc} alt=''/>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            ): (
-                <div></div>
-            )}
-            {/* {albumPhotos.AlbumPhotos.map(ele => {
-                console.log(ele)
-                return (
-                    <div></div>
-                )
-            })} */}
+                    ): (
+                        <div>
+                            
+                        </div>
+                    )}
+                {/* </div> */}
+            </div>
         </div>
     )
 }
