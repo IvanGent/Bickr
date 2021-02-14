89b2eb78ca22d0fa056d8d499a2721a023efb55c
IvanGent <gentilleivan@gmail.com>
2021-02-14 13:46:17 -0500

    showing album photos

--- frontend/src/components/ShowingAlbum/index.js
+++ frontend/src/components/ShowingAlbum/index.js
@@ -4,21 +4,39 @@ import { fetch } from '../../store/csrf';
 
 const ShowngAlbum = ({ album }) => {
     const [albumPhotos, setAlbum] = useState([]);
+    const [albumInfo, setAlbumInfo] = useState([]);
 
     useEffect(() => {
         (async () => {
             console.log(album)
             const res = await fetch(`/api/album/${album}`)
-            setAlbum(res.data.album);
+            setAlbumInfo(res.data.album);
+            setAlbum(res.data.album.AlbumPhotos);
             console.log(res.data.album)
         })()
 
-    }, [albumPhotos.length])
+    }, [])
 
     return (
         <div>
-            <h1>{albumPhotos.name}</h1>
-            
+            <h1>{albumInfo.name}</h1>
+            {albumPhotos.length > 0 ? (
+                <div>
+                    {albumPhotos.map(ele => (
+                        <div key={ele.Photo.id}>
+                            <img id={ele.Photo.id} src={ele.Photo.thumbSrc} alt='' />
+                        </div>
+                    ))}
+                </div>
+            ): (
+                <div></div>
+            )}
+            {/* {albumPhotos.AlbumPhotos.map(ele => {
+                console.log(ele)
+                return (
+                    <div></div>
+                )
+            })} */}
         </div>
     )
 }
