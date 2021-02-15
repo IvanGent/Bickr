81ae38d3aaa8094d2e0270d1c428cb509b9f8157
IvanGent <gentilleivan@gmail.com>
2021-02-15 12:10:53 -0500

    worked footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -7,12 +7,15 @@ import LinkedIn from '../../images/LinkedIn.png'
 const Footer = () => (
   <section className='footer'>
     <a href="https://github.com/IvanGent">
+      Github
       <img src={Github} alt='github' />
     </a>
     <a href="https://www.linkedin.com/in/ivan-gentille-181630200/">
+      LinkedIn
       <img src={LinkedIn} alt='LinkedIn' />
     </a>
     <a href="https://angel.co/u/ivan-gentille" >
+      AngelList
       <img src={AngelList} alt='angellist' />
     </a>
     <a href="https://github.com/IvanGent/Bickr/blob/master/README.md" >README</a>
