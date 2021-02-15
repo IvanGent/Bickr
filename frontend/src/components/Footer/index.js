7316c0dfad819ad6a4f75a1faaa14504b7bb5ff5
IvanGent <gentilleivan@gmail.com>
2021-02-15 12:07:54 -0500

    working with footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import './Footer.css';
+import Github from '../../images/Github.png'
+import AngelList from '../../images/angellist.png'
+import LinkedIn from '../../images/LinkedIn.png'
 
 const Footer = () => (
   <section className='footer'>
-    <a href="https://github.com/IvanGent/Bickr/blob/master/README.md" >About</a>
+    <a href="https://github.com/IvanGent/Bickr/blob/master/README.md" >README</a>
+    <a href="https://github.com/IvanGent">
+      <img src={Github} alt='github' />
+    </a>
+    <a href="https://www.linkedin.com/in/ivan-gentille-181630200/">
+      <img src={LinkedIn} alt='LinkedIn' />
+    </a>
+    <a href="https://angel.co/u/ivan-gentille" >
+      <img src={AngelList} alt='angellist' />
+    </a>
     {/* <footer>
       <a href="https://github.com/IvanGent/Bickr/blob/master/README.md">About</a>
     </footer> */}
