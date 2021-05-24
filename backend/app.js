73fa7ed910a7f298762fdb51ce9b3ef85f0f0420
IvanGent <gentilleivan@gmail.com>
2021-05-24 13:04:55 -0400

    trying to fix styling

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 
 app.use(cookieParser());
 app.use(express.json({limit: '50mb'}));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 // Security Middleware
 if (!isProduction) {
